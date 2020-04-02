919f10f1e611204a2f49d0d0d65d97e7a855a5ef
Christian Varisco <vari.chri1993@gmail.com>
2020-04-02 21:47:17 +0200

    Change name

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Router } from "preact-router";
 import "tailwindcss/dist/tailwind.min.css";
 
 // Routes
-import Home from "./routes/Home/index.js";
+import Home from "./routes/Home/home.js";
 import Form from "./routes/Form/index.js";
 
 import Navbar from "./components/Navbar.js";
